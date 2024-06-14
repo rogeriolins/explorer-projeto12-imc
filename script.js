d5d4b9d99aca13e9d9add3e09517aed50addf788
Rogerio Lins <rogerio_lins2002@yahoo.com>
2024-06-14 16:16:28 -0300

    Add functions js

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 // vars and consts
+const forms = document.querySelector("form")
+const inputWeight = document.querySelector("#peopleWeight")
+const inputHeight = document.querySelector("#peopleHeight")
+
+const modalWrapper = document.querySelector(".modal-wrapper")
+const modalMessage = document.querySelector(".modal-wrapper .title span")
+const modalBtnClose = document.querySelector(".modal button.close")
+
+forms.onsubmit = (event) => {
+  event.preventDefault()
+
+  const weight = inputWeight.value
+  const height = inputHeight.value
+
+  const result = imc(weight, height)
+  modalMessage.innerText = result
+
+  modalWrapper.classList.add("open")
+}
 
 // events
+modalBtnClose.addEventListener("click", function () {
+  modalWrapper.classList.remove("open")
+})
 
 // functions
+function imc(weight, height) {
+  return (weight / (height / 100) ** 2).toFixed(2)
+}
